3ed0b072636230cc795709982471d30124e9bed4
panggih mario <panggihmario@gmail.com>
2025-01-10 15:32:31 +0700

    add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import Input from "./components/input";
 import "./App.css";
 import Button from "./components/button";
-import Container from "./components/container";
+// import Container from "./components/container";
+import Form from "./components/form";
 
 function App() {
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+  }
+
   return (
-    <>
-      <Input id="username" label="Username" />
+    <main>
+      {/* <Input id="username" label="Username" />
       <Button>Submit Button</Button>
-      <Container as={Button}>Button Container</Container>
-    </>
+      <Container as={Button}>Button Container</Container> */}
+
+      <Form onSave={handleSave}>
+        <Input id="name" label="Name" type="text" />
+        <Input id="age" label="Age" type="number" />
+        <Button>Save</Button>
+      </Form>
+    </main>
   );
 }
 
